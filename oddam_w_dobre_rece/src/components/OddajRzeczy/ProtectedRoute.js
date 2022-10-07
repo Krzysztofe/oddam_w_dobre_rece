b9fb6cdc66f42cc872bcc563f3fa6cf4b31e23b2
kk <gitaragra@gmail.com>
2022-10-07 20:21:48 +0200

    protectedPath

--- oddam_w_dobre_rece/src/components/OddajRzeczy/ProtectedRoute.js
+++ oddam_w_dobre_rece/src/components/OddajRzeczy/ProtectedRoute.js
@@ -8,23 +8,23 @@ import Title from "../Title";
 const ProtectedRouteOddajMain  = () => {
     const [user] = useAuthState(auth);
 
-    // if (!user?.email) {
-    //     return (
-    //         <>
-    //             <div className='headerSpacer'></div>
-    //             <main className='wrapper wrapper--login'>
-    //                 <Title text1={'zaloguj się'}
-    //                        text2={''}
-    //                        classContainer={''}
-    //                        classH2={''}
-    //                        classUnderline={'login__underline'}/>
-    //                 <Link to='/'>
-    //                     <button className='buttonLogin'>strona główna</button>
-    //                 </Link>
-    //             </main>
-    //         </>
-    //     )
-    // }
+    if (!user?.email) {
+        return (
+            <>
+                <div className='headerSpacer'></div>
+                <main className='wrapper wrapper--login'>
+                    <Title text1={'zaloguj się'}
+                           text2={''}
+                           classContainer={''}
+                           classH2={''}
+                           classUnderline={'login__underline'}/>
+                    <Link to='/'>
+                        <button className='buttonLogin'>strona główna</button>
+                    </Link>
+                </main>
+            </>
+        )
+    }
     return (
         <OddajMain/>
     );
