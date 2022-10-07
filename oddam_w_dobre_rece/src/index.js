bc6a0093aaf08d8a4b4d24151d58c446063936c1
kk <gitaragra@gmail.com>
2022-10-07 19:02:34 +0200

    devDependencies

--- oddam_w_dobre_rece/src/index.js
+++ oddam_w_dobre_rece/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {HashRouter} from 'react-router-dom'
 import './scss/index.scss';
 import App from './components/App';
 
@@ -7,6 +8,6 @@ import App from './components/App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+          <App />
   </React.StrictMode>
 );
