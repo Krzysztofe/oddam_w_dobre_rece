50ca905015372b1f8e60a4ab0cbbaf263e01d5b9
kk <gitaragra@gmail.com>
2022-10-07 19:24:31 +0200

    s

--- oddam_w_dobre_rece/src/components/App.js
+++ oddam_w_dobre_rece/src/components/App.js
@@ -8,16 +8,18 @@ import ProtectedRouteOddajMain from "./OddajRzeczy/ProtectedRoute";
 
 function App() {
     return (
+    <>
         <BrowserRouter>
             <NavBar/>
             <Routes>
-                <Route path='/Oddam_w_dobre_rece' element={<Home/>}/>
+                <Route path='/' element={<Home/>}/>
                 <Route path='/rejestracja' element={<Register/>}/>/
                 <Route path='/logowanie' element={<Login/>}/>
                 <Route path='/wylogowano' element={<Logout/>}/>
                 <Route path='/oddaj' element={<ProtectedRouteOddajMain/>}/>
             </Routes>
-        </BrowserRouter>
+         </BrowserRouter>
+    </>
     );
 }
 
