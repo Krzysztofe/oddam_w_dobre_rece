5843f6722edc045ad921c50cdea471b1ec4cebc2
kk <gitaragra@gmail.com>
2022-10-28 23:24:01 +0200

    dodanie width 100

--- oddam_w_dobre_rece/src/components/Home/Section_5/Section_5.tsx
+++ oddam_w_dobre_rece/src/components/Home/Section_5/Section_5.tsx
@@ -10,9 +10,7 @@ const Section_5:FC <Props> = ({children}) => {
         <section className="wrapper wrapper--section-5" id='contact'>
             <div className="section-5__opacity">
                 <aside className="section-5__right">
-
                     {children}
-
                 </aside>
             </div>
         </section>
--- oddam_w_dobre_rece/src/components/OddajRzeczy/OddajMain.tsx
+++ oddam_w_dobre_rece/src/components/OddajRzeczy/OddajMain.tsx
@@ -1,16 +1,16 @@
 import React, {useContext, FC} from "react";
 import OddajHeader from "./OddajHeader/OddajHeader";
-import Form from "../Home/Section_5/Section_5_Form";
 import OddajContainer_1 from "./OddajContainers/OddajContainer_1";
 import OddajContainer_2 from "./OddajContainers/OddajContainer_2";
 import OddajContainer_3 from "./OddajContainers/OddajContainer_3";
 import OddajContainer_4 from "./OddajContainers/OddajContainer_4";
 import OddajContainerSummary from "./OddajContainers/OddajContainerSummary";
-import Title from "../Title";
 import OddajContainerThnx from "./OddajContainers/OddajContainerThnx";
 import Section_5 from "../Home/Section_5/Section_5";
 import Footer from "../Home/Footer";
 import {GlobalContext} from "./OddajContext/GlobalContextProv";
+import Title from "../Title";
+import Form from "../Home/Section_5/Section_5_Form";
 
 
 
@@ -30,6 +30,11 @@ const OddajMain: FC = () => {
             {counter === 4 && <OddajContainer_4/>}
             {counter === 5 && <OddajContainerSummary/> }
             {counter === 6 && <OddajContainerThnx/>}
+            <Section_5>
+                <Title text={['skontaktuj się z nami']}
+                       classUnderline={'section-5__decoration'}/>
+                <Form/>
+            </Section_5>
             <Footer/>
         </>
     );
